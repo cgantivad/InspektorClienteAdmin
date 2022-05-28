4a28cee2e95e9ccc5416905af4a11dc723e99173
Sebastian Balanta Zamora <sebasbz@hotmail.com>
2022-05-28 18:24:17 -0500

    trabajando

--- src/app/pages/restrictive-lists/list-group/list-group-form/list-group-form.component.ts
+++ src/app/pages/restrictive-lists/list-group/list-group-form/list-group-form.component.ts
@@ -4,13 +4,14 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Location} from '@angular/common';
 import {ListGroupService} from '../../backend/common/services/list-group.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {ListGroup} from "../../interfaces/list-group";
+import {ListGroup} from '../../interfaces/list-group';
 
 @Component({
     selector: 'ngx-list-group-form',
     templateUrl: './list-group-form.component.html',
     styleUrls: ['./list-group-form.component.scss'],
 })
+
 export class ListGroupFormComponent implements OnInit {
 
     constructor(protected toastrService: NbToastrService,
@@ -23,9 +24,8 @@ export class ListGroupFormComponent implements OnInit {
 
     formGroup: FormGroup;
     nameError = null;
-    sourceError = null;
-
-    //weightError = null;
+    descriptionError = null;
+    colorError = null;
     headerTitle = 'CREAR GRUPO';
 
     get name() {
@@ -68,7 +68,7 @@ export class ListGroupFormComponent implements OnInit {
                         color: listGroup.color ? listGroup.color : '',
                     });
                 }, error => {
-                    this.toastrService.danger('', 'Usuario no econtrado', {
+                    this.toastrService.danger('', 'Registro no econtrado', {
                         icon: '',
                     });
                 });
@@ -76,12 +76,51 @@ export class ListGroupFormComponent implements OnInit {
     }
 
     save() {
-        const data: ListGroup = this.formGroup.value;
-        console.log(data);
+        const id = this.route.snapshot.paramMap.get('id');
+        const user: ListGroup = this.formGroup.value;
+
+        if (id == null) {
+            this.listGroupService.create(user).subscribe(() => {
+                this.formGroup.reset();
+                this.toastrService.success('', '¡Registro exitoso!', {
+                    icon: '',
+                });
+                this.clearErrors();
+            }, errorResponse => {
+                this.setupErrorResponse(errorResponse);
+            });
+        } else {
+            this.listGroupService.update(+id, user).subscribe((record) => {
+                this.toastrService.success('', '¡Registro exitoso!', {
+                    icon: '',
+                });
+                this.clearErrors();
+            }, errorResponse => {
+                this.setupErrorResponse(errorResponse);
+            });
+        }
     }
 
     back($event: MouseEvent) {
         $event.preventDefault();
         this.location.back();
+    }
+
+    private setupErrorResponse(errorResponse) {
+        const errorObj = errorResponse.error.errors;
+        if (errorObj.hasOwnProperty('name'))
+            this.nameError = errorObj.name;
+
+        if (errorObj.hasOwnProperty('description'))
+            this.descriptionError = errorObj.description;
+
+        if (errorObj.hasOwnProperty('color'))
+            this.colorError = errorObj.color;
+    }
+
+    private clearErrors() {
+        this.nameError = null;
+        this.descriptionError = null;
+        this.colorError = null;
     }
 }
